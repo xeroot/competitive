65672892f2b3fcf432d4ab8c538ca4e1ee60180e
xeroot <r10500@hotmail.com>
2020-01-23 15:26:07 -0500

    first commit
    :)

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long //10^64
+#define ull unsigned long long //10^65
+using namespace std;
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  
+  // code here ...
+
+  return 0;
+}
